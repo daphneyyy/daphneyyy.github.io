a28a6f5a5cd29f8441599c297a8e9008635a2b54
Xuewen Yang <xuewenyang1@gmail.com>
2024-11-02 21:23:56 -0700

    modified

---
+++ js/scripts-project.js
@@ -0,0 +1,30 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const project_snapshot = document.querySelectorAll(".project-snapshot");
+    
+
+//     // add hover functionality to project snapshot
+//     project_snapshot.forEach(function (snapshot) {
+//       const buttons = snapshot.querySelectorAll(".pj");
+//         snapshot.addEventListener("mouseover", function () {            
+//             buttons.forEach(function (button) {
+//                 button.style.display = "block";
+//             });
+
+//             // snapshot.classList.add("project-snapshot-hover");
+
+//             // const button = snapshot.querySelector(".pj");
+//             // button.addEventListener("click", function () {
+//                 // get the project link
+//                 // const project_link = snapshot.querySelector(".project-snapshot-link");
+//                 // window.open(project_link.href, '_blank');
+//         });
+
+//         snapshot.addEventListener("mouseout", function () {
+//             buttons.forEach(function (button) {
+//                 button.style.display = "none";
+//             });
+
+//             // snapshot.classList.remove("project-snapshot-hover");
+//         });
+//     });
+// });
